75bbd2a54bc6bec46404a93c256b680a966c594a
wangkaiwd <1162486176@qq.com>
2020-04-22 11:12:14 +0800

    WS-2020.1 <wangkai@wangkaideMacBook-Pro.local Update baseRefactoring.xml, ide.general.xml, React.xml, Default copy.xml, _@user_Material Darker.icls, Default.xml, ui.lnf.xml, workspace.xml, filetypes.xml, vcs.xml, _@user_Darcula.icls, editor.xml, applicationLibraries.xml, github.xml	Create _@user_Material Solarized Dark.icls, _@user_Nord.icls, Vue Single File Component.vue, _@user_Atom One Dark.icls, print.xml, git.xml, _@user_Material Palenight.icls, _@user_Atom One Light.icls, updates.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="${COMPONENT_NAME}">
+    #[[$END$]]#
+  </div> 
+</template>
+
+<script>
+export default {
+  name: "${COMPONENT_NAME}",
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .${COMPONENT_NAME} {
+      
+  }
+</style>
